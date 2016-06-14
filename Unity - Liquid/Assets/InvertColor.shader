8a4ac4cf6da18121036061839eb0a903aca6b1bf
Pablo Bollansée <pablo.bollansee@gmail.com>
2016-06-14 19:53:50 +0200

    A simple color-inversion test.

---
+++ Unity - Liquid/Assets/InvertColor.shader
@@ -0,0 +1,32 @@
+﻿Shader "Hidden/InvertColor"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+	}
+
+		SubShader
+	{
+		// No culling or depth
+		Cull Off ZWrite Off ZTest Always
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert_img
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			uniform sampler2D _MainTex;
+			
+			fixed4 frag(v2f_img i) : COLOR
+			{
+				fixed4 c = tex2D(_MainTex, i.uv);
+				return 1 - c;
+			}
+			
+			ENDCG
+		}
+	}
+}
---
+++ Unity - Liquid/Assets/Simulation.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Simulation : MonoBehaviour
+{
+	public Texture _testInitialTexture;
+	public RenderTexture _testTexture;
+	private RenderTexture _testBuffer;
+	public Material _testMaterial;
+
+	public float _updateInterval = 1f;
+	private float _lastUpdated;
+	
+	void Start ()
+	{
+		Graphics.Blit(_testInitialTexture, _testTexture);
+		
+		_testBuffer = new RenderTexture(
+			_testTexture.width, _testTexture.height,
+			_testTexture.depth, _testTexture.format);
+
+		_lastUpdated = Time.time;
+	}
+	
+	void Update ()
+	{
+		if (Time.time >= _lastUpdated + _updateInterval)
+		{
+			UpdateSimulation();
+			_lastUpdated = Time.time;
+		}
+	}
+
+	void UpdateSimulation()
+	{
+		Graphics.Blit(_testTexture, _testBuffer, _testMaterial);
+		Graphics.Blit(_testBuffer, _testTexture);
+	}
+}
