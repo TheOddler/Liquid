c7332cf2aaada87b7a909ad8f3701b9d799c2fd6
Pablo Bollansée <pablo.bollansee@gmail.com>
2016-06-16 21:33:07 +0200

    Simple adding of water and sand.

--- Unity - Liquid/Assets/Helpers/TextureUtil.cs
+++ Unity - Liquid/Assets/Helpers/TextureUtil.cs
@@ -1,9 +1,8 @@
 ﻿using UnityEngine;
-using System.Collections;
 
 public static class TextureUtil
 {
-	static public Texture GetBlackTexture(int width, int height, TextureFormat format, bool mipmap = false, bool linear = true)
+	static public Texture2D GetBlackTexture(int width, int height, TextureFormat format, bool mipmap = false, bool linear = true)
 	{
 		var texture = new Texture2D(1, 1, format, mipmap, linear);
 		texture.SetPixel(0, 0, Color.black);
--- Unity - Liquid/Assets/Simulation/Simulation.cs
+++ Unity - Liquid/Assets/Simulation/Simulation.cs
@@ -16,6 +16,11 @@ public class Simulation : MonoBehaviour
 	[SerializeField]
 	Texture _initialWaterSandRock;
 
+	[SerializeField]
+	float _addingSpeed = 0.2f;
+	[SerializeField]
+	int _addingBrushSize = 15;
+
 	//
 	// Schaders
 	// ---
@@ -33,7 +38,8 @@ public class Simulation : MonoBehaviour
 	// ---
 	BufferedRenderTexture _waterSandRock;
 
-	Texture _sourceWaterSandRock;
+	Texture2D _clearSourceWaterSandRock;
+	Texture2D _sourceWaterSandRock;
 
 	//
 	// Other
@@ -58,6 +64,7 @@ public class Simulation : MonoBehaviour
 		var readWrite = RenderTextureReadWrite.Linear;
 		_waterSandRock = new BufferedRenderTexture(_size, _size, 0, format, readWrite);
 
+		_clearSourceWaterSandRock = TextureUtil.GetBlackTexture(_size, _size, TextureFormat.RGBAFloat);
 		_sourceWaterSandRock = TextureUtil.GetBlackTexture(_size, _size, TextureFormat.RGBAFloat);
 
 		// Further initialization
@@ -70,6 +77,29 @@ public class Simulation : MonoBehaviour
 	
 	void Update ()
 	{
+		bool addWater = Input.GetMouseButton(0);
+		bool addSand = Input.GetMouseButton(1);
+		if (addWater || addSand)
+		{
+			int halfSize = _addingBrushSize / 2;
+
+			int x = Mathf.RoundToInt(Input.mousePosition.x) * _sourceWaterSandRock.width / Screen.width;
+			int y = Mathf.RoundToInt(Input.mousePosition.y) * _sourceWaterSandRock.height / Screen.height;
+			x = Mathf.Clamp(x, halfSize, _sourceWaterSandRock.width - halfSize - 1);
+			y = Mathf.Clamp(y, halfSize, _sourceWaterSandRock.height - halfSize - 1);
+
+			var color = _sourceWaterSandRock.GetPixel(x, y);
+			if (addWater) color.r += _addingSpeed * Time.deltaTime; // water
+			if (addSand) color.g += _addingSpeed * Time.deltaTime; // sand
+			//color.b += 0; // rock
+
+			Color32[] colors = new Color32[_addingBrushSize * _addingBrushSize];
+			for (int i = 0; i < colors.Length; ++i)
+				colors[i] = color;
+			_sourceWaterSandRock.SetPixels32(x - halfSize, y - halfSize, _addingBrushSize, _addingBrushSize, colors);
+			_sourceWaterSandRock.Apply();
+		}
+
 		if (Time.time >= _lastUpdated + _updateInterval)
 		{
 			UpdateSimulation();
@@ -93,5 +123,6 @@ public class Simulation : MonoBehaviour
 	{
 		_addSourceMaterial.SetTexture("_SourceTex", _sourceWaterSandRock);
 		Graphics.Blit(_waterSandRock.Texture, _waterSandRock.Buffer, _addSourceMaterial);
+		_sourceWaterSandRock.SetPixels32(_clearSourceWaterSandRock.GetPixels32());
 	}
 }
